e712442bebbabce68c13921e1058085527338b40
= <=>
2023-10-31 09:44:30 +0500

    fixed

--- postgres_n124/src/router/orders.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const middleware_1 = require("../middleware");
-const orders_1 = require("../controller/orders");
-const routerOrders = (0, express_1.Router)();
-routerOrders.post('/orders', middleware_1.GetMid, orders_1.addOrders);
-exports.default = routerOrders;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,4 @@
 import { Sequelize } from "sequelize";
 
-export const newSequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    database: 'n124',
-    password: 'mirzafar123',
-    username: 'postgres',
-    logging: false
-});
+export const newSequelize = new Sequelize('postgres://postgress:pIql5aW4FvYIohY3lrp9zP87CaxuL8GJ@dpg-ckv3lvramefc739necs0-a/n124_39of');
 
